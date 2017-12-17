b17ba5b3d340752d2fb3c24f6929954b616ae8f0
nvertigo <nvertigo67@gmail.com>
2017-12-17 11:05:22 +0100

    mcpu: fix nested ifeq.
    Change-Id: I79fb9134b4d57014ba3fde80e47b02adf32382bc

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,12 +1,11 @@
-ifeq ($(TARGET_USE_SDCLANG),true)
 ifneq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+ifeq ($(TARGET_USE_SDCLANG),true)
 	arch_variant_cflags := -mcpu=cortex-a57
-else
+endif
 else
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
 else
 	arch_variant_cflags :=
-endif
 endif
 endif
