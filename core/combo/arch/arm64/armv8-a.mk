ac1c09f78fe488732ec20894b4f277dc536a52f0
nvertigo <nvertigo67@gmail.com>
2017-12-30 09:49:09 +0100

    mcpu: use cortex-a53 with stock clang/llvm.
    Change-Id: I7d70c64756192b311c0e949219bbe911a47d30eb

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,6 +1,8 @@
 ifneq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 ifeq ($(TARGET_USE_SDCLANG),true)
 	arch_variant_cflags := -mcpu=cortex-a57
+else
+	arch_variant_cflags := -mcpu=cortex-a53
 endif
 else
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
